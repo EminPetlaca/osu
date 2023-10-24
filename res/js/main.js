56fb0fd88ff24ab394c2518179f33b401a4fa4d8
EminPetlaca <eminpetlacanpp@gmail.com>
2023-10-24 15:26:46 +0200

    osu

---
+++ res/js/main.js
@@ -0,0 +1,39 @@
+const point = document.getElementById("point");
+const startGame = document.getElementById("startGame");
+
+let pointInterval;
+
+
+startGame.onclick = () => {
+    startGame.style.display = "none";
+    startPointInterval()
+}
+
+const movePoint = (element, x, y) => {
+    element.style.top = `${y}px`
+    element.style.left = `${x}px`
+}
+
+const startPointInterval = () => {
+    clearInterval(pointInterval);
+    setPointClick(point);  
+    pointInterval = setInterval(() => {
+        setSize(point, getRandomNumber(40, 70));
+        movePoint(point, getRandomNumber(100,600), getRandomNumber(100,600));
+    }, 2500);
+};
+
+
+const getRandomNumber = (minimum, maximum) => (Math.random() * (maximum - minimum + 1)) << 0
+
+const setPointClick = (element) => {
+    element.onclick = () => {
+        element.innerText++;
+    }
+}
+
+const setSize = (element, size) => {
+    element.style.width = `${size}px`;
+    element.style.height = `${size}px`;
+}
+
